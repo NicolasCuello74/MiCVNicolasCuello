85cd4437149d7d11f7b8d143e58476a61e2f0af7
NicoCuello <nicocuello7@gmail.com>
2023-02-28 15:38:16 -0300

    se agregaron archivos

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+window.onscroll = function() { efectoHabilidades() };
+
+function efectoHabilidades() {
+    var habilidades = document.getElementById("habilidades");
+    var distancia_habilidades = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if (distancia_habilidades >=300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
